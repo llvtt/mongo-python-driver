b67d658abba5de7ccf05c66ec99a65faa3c3f7ff
Luke Lovett <luke.lovett@mongodb.com>
2014-07-31 15:08:49 +0000

    fix up C extensions per bernies comments

--- bson/_cbsonmodule.c
+++ bson/_cbsonmodule.c
@@ -555,6 +555,7 @@ static int _write_element_to_buffer(PyObject* self, buffer_t buffer,
                                     unsigned char uuid_subtype) {
     struct module_state *state = GETSTATE(self);
     PyObject* type_marker = NULL;
+    PyObject* mapping_type;
     PyObject* uuid_type;
 
     /*
@@ -872,13 +873,6 @@ static int _write_element_to_buffer(PyObject* self, buffer_t buffer,
     } else if (value == Py_None) {
         *(buffer_get_buffer(buffer) + type_byte) = 0x0A;
         return 1;
-    } else if (PyObject_IsInstance(value, state->Mapping)) {
-        // PyObject_IsInstance returns -1 on error
-        if (PyErr_Occurred()) {
-            return 0;
-        }
-        *(buffer_get_buffer(buffer) + type_byte) = 0x03;
-        return write_dict(self, buffer, value, check_keys, uuid_subtype, 0);
     } else if (PyList_Check(value) || PyTuple_Check(value)) {
         Py_ssize_t items, i;
         int start_position,
@@ -1029,11 +1023,21 @@ static int _write_element_to_buffer(PyObject* self, buffer_t buffer,
     }
     
     /* 
-     * Try UUID last since we have to import
-     * it if we're in a sub-interpreter.
+     * Try Mapping and UUID last since we have to import
+     * them if we're in a sub-interpreter.
      */
     uuid_type = _get_object(state->UUID, "uuid", "UUID");
-    if (uuid_type && PyObject_IsInstance(value, uuid_type)) {
+    mapping_type = _get_object(state->Mapping, "collections", "Mapping");
+
+    if (mapping_type && PyObject_IsInstance(value, mapping_type)) {
+        // PyObject_IsInstance returns -1 on error
+        if (PyErr_Occurred()) {
+            return 0;
+        }
+        Py_DECREF(mapping_type);
+        *(buffer_get_buffer(buffer) + type_byte) = 0x03;
+        return write_dict(self, buffer, value, check_keys, uuid_subtype, 0);
+    } else if (uuid_type && PyObject_IsInstance(value, uuid_type)) {
         /* Just a special case of Binary above, but
          * simpler to do as a separate case. */
         PyObject* bytes;
@@ -1097,6 +1101,7 @@ static int _write_element_to_buffer(PyObject* self, buffer_t buffer,
         Py_DECREF(bytes);
         return 1;
     }
+    Py_XDECREF(mapping_type);
     Py_XDECREF(uuid_type);
     /* We can't determine value's type. Fail. */
     _set_cannot_encode(value);
@@ -1311,13 +1316,56 @@ int write_dict(PyObject* self, buffer_t buffer,
     char zero = 0;
     int length;
     int length_location;
-    struct module_state *state = GETSTATE(self);
 
-    if (PyObject_IsInstance(dict, state->Mapping) != 1) {
-        // PyObject_IsInstance return -1 on error
-        if (PyErr_Occurred()) {
+    length_location = buffer_save_space(buffer, 4);
+    if (length_location == -1) {
+        PyErr_NoMemory();
+        return 0;
+    }
+
+    /* Write _id first if this is a top level doc. */
+    if (top_level && PyMapping_HasKeyString(dict, "_id")) {
+        PyObject* _id = PyMapping_GetItemString(dict, "_id");
+        if (!write_pair(self, buffer, "_id", 3,
+                        _id, check_keys, uuid_subtype, 1)) {
             return 0;
         }
+    }
+
+    iter = PyObject_GetIter(dict);
+    if (iter == NULL) {
+        goto pydict_error;
+    }
+    while ((key = PyIter_Next(iter)) != NULL) {
+        PyObject* value = PyObject_GetItem(dict, key);
+        if (PyErr_Occurred()) goto pydict_error;
+        if (!value) {
+            PyErr_SetObject(PyExc_KeyError, key);
+            Py_DECREF(key);
+            Py_DECREF(iter);
+            return 0;
+        }
+        if (!decode_and_write_pair(self, buffer, key, value,
+                                   check_keys, uuid_subtype, top_level)) {
+            Py_DECREF(key);
+            Py_DECREF(iter);
+            return 0;
+        }
+        Py_DECREF(key);
+    }
+    Py_DECREF(iter);
+    if (PyErr_Occurred()) goto pydict_error;
+
+    /* write null byte and fill in length */
+    if (!buffer_write_bytes(buffer, &zero, 1)) {
+        return 0;
+    }
+    length = buffer_get_position(buffer) - length_location;
+    memcpy(buffer_get_buffer(buffer) + length_location, &length, 4);
+    return 1;
+
+pydict_error:
+    if (PyErr_Occurred()) {
         PyObject* repr = PyObject_Repr(dict);
         if (repr) {
 #if PY_MAJOR_VERSION >= 3
@@ -1350,53 +1398,8 @@ int write_dict(PyObject* self, buffer_t buffer,
             PyErr_SetString(PyExc_TypeError,
                             "encoder expected a mapping type");
         }
-        return 0;
     }
-
-    length_location = buffer_save_space(buffer, 4);
-    if (length_location == -1) {
-        PyErr_NoMemory();
-        return 0;
-    }
-
-    /* Write _id first if this is a top level doc. */
-    if (top_level && PyMapping_HasKeyString(dict, "_id")) {
-        PyObject* _id = PyMapping_GetItemString(dict, "_id");
-        if (!write_pair(self, buffer, "_id", 3,
-                        _id, check_keys, uuid_subtype, 1)) {
-            return 0;
-        }
-    }
-
-    iter = PyObject_GetIter(dict);
-    if (iter == NULL) {
-        return 0;
-    }
-    while ((key = PyIter_Next(iter)) != NULL) {
-        PyObject* value = PyObject_GetItem(dict, key);
-        if (!value) {
-            PyErr_SetObject(PyExc_KeyError, key);
-            Py_DECREF(key);
-            Py_DECREF(iter);
-            return 0;
-        }
-        if (!decode_and_write_pair(self, buffer, key, value,
-                                   check_keys, uuid_subtype, top_level)) {
-            Py_DECREF(key);
-            Py_DECREF(iter);
-            return 0;
-        }
-        Py_DECREF(key);
-    }
-    Py_DECREF(iter);
-
-    /* write null byte and fill in length */
-    if (!buffer_write_bytes(buffer, &zero, 1)) {
-        return 0;
-    }
-    length = buffer_get_position(buffer) - length_location;
-    memcpy(buffer_get_buffer(buffer) + length_location, &length, 4);
-    return 1;
+    return 0;
 }
 
 static PyObject* _cbson_dict_to_bson(PyObject* self, PyObject* args) {
