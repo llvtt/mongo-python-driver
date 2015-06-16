a8e4abd4f6e7f524b0bd11fa0761963ddd9248f0
Luke Lovett <luke.lovett@mongodb.com>
2015-06-16 19:36:40 +0000

    Revert "Adding stuff to _cbson_traverse and _cbson_clear. Not sure if it's supposed to be there or not."
    This reverts commit f271977423167c15195e7b30d16fe32475a7cce7.

--- bson/_cbsonmodule.c
+++ bson/_cbsonmodule.c
@@ -2623,9 +2623,6 @@ static int _cbson_traverse(PyObject *m, visitproc visit, void *arg) {
     Py_VISIT(GETSTATE(m)->MaxKey);
     Py_VISIT(GETSTATE(m)->UTC);
     Py_VISIT(GETSTATE(m)->REType);
-    Py_VISIT(GETSTATE(m)->BSONInt64);
-    Py_VISIT(GETSTATE(m)->Mapping);
-    Py_VISIT(GETSTATE(m)->RawBSONDocument);
     return 0;
 }
 
@@ -2641,9 +2638,6 @@ static int _cbson_clear(PyObject *m) {
     Py_CLEAR(GETSTATE(m)->MaxKey);
     Py_CLEAR(GETSTATE(m)->UTC);
     Py_CLEAR(GETSTATE(m)->REType);
-    Py_VISIT(GETSTATE(m)->BSONInt64);
-    Py_VISIT(GETSTATE(m)->Mapping);
-    Py_VISIT(GETSTATE(m)->RawBSONDocument);
     return 0;
 }
 
